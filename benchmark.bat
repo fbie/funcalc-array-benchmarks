684030580d44d6160619c8a0ea3afb42349078a0
Florian Biermann <fbie@itu.dk>
2017-08-08 11:33:04 +0200

    Initial commit

---
+++ benchmark.bat
@@ -0,0 +1,18 @@
+mkdir logs\full\arrays
+mkdir logs\full\cells
+mkdir logs\full\seq
+
+git checkout origin/fbie/cell-array-transformation
+build -c
+build -r -n
+for /f "usebackq delims=|" %f in (`dir /b /s F:\funcalc-euses ^| findstr /i "\.xml$"`) do funcalc -r full 100 %f 1> logs\arrays\%~nxf.out 2> logs\arrays\%~nxf.err
+
+git checkout origin/fbie/parallel-full-recalc
+build -c
+build -r -n
+for /f "usebackq delims=|" %f in (`dir /b /s F:\funcalc-euses ^| findstr /i "\.xml$"`) do funcalc -r full 100 %f 1> logs\cells\%~nxf.out 2> logs\cells\%~nxf.err
+
+git checkout origin/parallel
+build -c
+build -r -n
+for /f "usebackq delims=|" %f in (`dir /b /s F:\funcalc-euses ^| findstr /i "\.xml$"`) do funcalc -r full 100 %f 1> logs\seq\%~nxf.out 2> logs\seq\%~nxf.err
