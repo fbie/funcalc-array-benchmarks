78b7946335a1e2979c6dd3f467c78b42a7e6f8a4
Florian Biermann <fbie@itu.dk>
2017-08-08 11:57:01 +0200

    Replace F: by ..

--- benchmark.bat
+++ benchmark.bat
@@ -5,14 +5,14 @@ mkdir logs\full\seq
 git checkout origin/fbie/cell-array-transformation
 build -c
 build -r -n
-for /f "usebackq delims=|" %f in (`dir /b /s F:\funcalc-euses ^| findstr /i "\.xml$"`) do funcalc -r full 100 %f 1> logs\arrays\%~nxf.out 2> logs\arrays\%~nxf.err
+for /f "usebackq delims=|" %f in (`dir /b /s ..\funcalc-euses ^| findstr /i "\.xml$"`) do funcalc -r full 100 %f 1> logs\arrays\%~nxf.out 2> logs\arrays\%~nxf.err
 
 git checkout origin/fbie/parallel-full-recalc
 build -c
 build -r -n
-for /f "usebackq delims=|" %f in (`dir /b /s F:\funcalc-euses ^| findstr /i "\.xml$"`) do funcalc -r full 100 %f 1> logs\cells\%~nxf.out 2> logs\cells\%~nxf.err
+for /f "usebackq delims=|" %f in (`dir /b /s ..\funcalc-euses ^| findstr /i "\.xml$"`) do funcalc -r full 100 %f 1> logs\cells\%~nxf.out 2> logs\cells\%~nxf.err
 
 git checkout origin/parallel
 build -c
 build -r -n
-for /f "usebackq delims=|" %f in (`dir /b /s F:\funcalc-euses ^| findstr /i "\.xml$"`) do funcalc -r full 100 %f 1> logs\seq\%~nxf.out 2> logs\seq\%~nxf.err
+for /f "usebackq delims=|" %f in (`dir /b /s ..\funcalc-euses ^| findstr /i "\.xml$"`) do funcalc -r full 100 %f 1> logs\seq\%~nxf.out 2> logs\seq\%~nxf.err
