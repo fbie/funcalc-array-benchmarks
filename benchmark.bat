63dafeb8dd12228b22474a0a86050cfa0b52e0da
Florian Biermann <fbie@itu.dk>
2018-01-31 10:43:11 +0100

    Update benchmark script to Puncalc variant and modify accordingly

--- benchmark.bat
+++ benchmark.bat
@@ -1,27 +1,24 @@
 @echo off
 if "%1" == "" goto :help
 if "%2" == "" goto :help
+if "%3" == "" goto :help
 
-:: Baseline
-call git checkout origin/parallel
-call :benchmark %1 %2 logs\seq
-
-:: Parallel task per cell
-call git checkout origin/fbie/parallel-full-recalc
-call :benchmark %1 %2 logs\cells
-
-:: Lifted cell arrays
 call git checkout origin/fbie/cell-array-transformation
-call :benchmark %1 %2 logs\arrays
+
+:: Sequential
+call :benchmark %1 %2 %3\seq -n
+call :benchmark %1 %2 %3\seq-array-seq -n -w
+call :benchmark %1 %2 %3\seq-array-par -n -w -p
 
 exit /b
 
 :help
 echo Usage:
-echo   benchmark.bat path\to\sheets iterations
+echo   benchmark.bat path\to\sheets iterations path\to\logs
 echo.
 echo path\to\sheets - Path to a folder that contains XML spreadsheets.
 echo iterations     - Number of iterations to repeat.
+echo path\to\logs   - Path to a folder where log files will be stored.
 echo.
 
 :: Done
@@ -32,16 +29,19 @@ setlocal
 set files=%1
 set n=%2
 set log=%3
+shift /3
+set flags=%*
 mkdir %log%
 
 :: Log build events
 echo Building...
-call build -c    >  %log%\build.log 2>&1
-call build -r -n >> %log%\build.log 2>&1
+git show -q           >  %log%\git.log
+call build -c         >  %log%\build.log 2>&1
+call build -r %flags% >> %log%\build.log 2>&1
 
-echo Benchmarking...
 :: Benchmark Funcalc for each sheet.
 for /r %files% %%I in (*.xml) do (
+    echo Benchmarking %%I
     call funcalc -r full %n% "%%I" 1> "%log%\%%~nxI.out" 2> "%log%\%%~nxI.err"
 )
 
